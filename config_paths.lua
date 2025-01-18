6dca1c31a26f47a7a521d19e3e5ed3f52d071178
Dylan Meunier <dylan19170@gmail.com>
2025-01-18 09:17:05 +0100

    feat : fix commmands and paths win

--- config_paths.lua
+++ config_paths.lua
@@ -5,42 +5,40 @@ local M = {}
 
 -- Base directories
 M.base = {
-    config = platform.join_path(platform.config_dir, "wezterm"),
-    documents = platform.documents_dir,
-    home = platform.home_dir,
-    music = platform.join_path(platform.home_dir, platform.is_windows and "Music" or "Musique"),
+	config = platform.join_path(platform.config_dir, "wezterm"),
+	documents = platform.documents_dir,
+	home = platform.home_dir,
+	music = platform.join_path(platform.home_dir, platform.is_windows and "Nextcloud\\Musique"),
 }
 
 -- Application directories
 M.apps = {
-    notes = platform.join_path(M.base.documents, "Notes"),
-    config = M.base.config,
-    music = M.base.music,
+	notes = platform.join_path(M.base.documents, "Notes"),
+	config = M.base.config,
+	music = M.base.music,
 }
 
 -- Commands
 M.commands = {
-    -- Editor commands
-    editor = platform.is_windows and "nvim.exe" or "nvim",
-    
-    -- Music player commands
-    music_player = platform.is_windows and {"explorer.exe"} or {"termusic"},
-    
-    -- File search commands
-    file_search = platform.is_windows 
-        and "rg.exe --files --type md" 
-        or "rg --files --type md",
+	-- Editor commands
+	editor = platform.is_windows and "nvim.exe" or "nvim",
+
+	-- Music player commands
+	music_player = platform.is_windows and { "termusic" },
+
+	-- File search commands
+	file_search = platform.is_windows and "rg.exe --files --type md" or "rg --files --type md",
 }
 
 -- Shell configuration
 M.shell = {
-    -- Default shell command
-    command = platform.default_shell,
-    
-    -- Shell initialization files
-    rc_file = platform.is_windows 
-        and platform.join_path(M.base.home, "Documents", "PowerShell", "Microsoft.PowerShell_profile.ps1")
-        or platform.join_path(M.base.home, ".zshrc"),
+	-- Default shell command
+	command = platform.default_shell,
+
+	-- Shell initialization files
+	rc_file = platform.is_windows
+			and platform.join_path(M.base.home, "Documents", "PowerShell", "Microsoft.PowerShell_profile.ps1")
+		or platform.join_path(M.base.home, ".zshrc"),
 }
 
-return M 
+return M
